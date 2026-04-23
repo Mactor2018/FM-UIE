28ead5b15fe592a432650188fc6db4938db67a60
YuruiZhu <mactor1024@gmail.com>
2026-04-23 21:21:38 +0800

    fix: adjust batch size to 16 to avoid OOM

--- scripts/ddp_train.sh
+++ scripts/ddp_train.sh
@@ -3,7 +3,7 @@ cd /home/zyr/RL-UIE/conditional-flow-matching
 torchrun --standalone --nnodes=1 --nproc_per_node=2 \
     train_uie.py \
     --parallel True \
-    --batch_size 32 \
+    --batch_size 16 \
     --lr 2e-4 \
     --total_steps 400001 \
     --val_step 5000 \
